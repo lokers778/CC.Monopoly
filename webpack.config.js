9884b5f5a990c14b39333f92a87aaefd78a5ce30
Łukasz <lokers778@gmail.com>
2019-08-25 00:51:37 +0200

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+var MiniCSS = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: ["./js/main.js","./scss/index.scss"],
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "build")
+    },
+    mode: 'development',
+    watch: true,
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+
+            }
+        },
+            {
+                test: /\.scss$/,
+                    use: [ MiniCSS.loader,'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCSS({
+            filename: "app.css",
+        })
+    ]
+}
