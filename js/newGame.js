bd0585f33ffedc9e2d0b228168ecd9ffee9348e1
Marcin Sala <toriuke44@gmail.com>
2019-09-03 20:46:06 +0200

    Change in the concept of adding players in newGame popup

--- js/newGame.js
+++ js/newGame.js
@@ -1,6 +1,5 @@
-const addPlayer = document.querySelector('.addPlayerSubmit');
 const playersList = document.querySelector('.playersList');
-const inputText = document.querySelector('.inputText');
+const btnNumberOfPlayers = [...document.querySelectorAll('.createNewPlayers')];
 
 const clearNode = node => {
   while (node.firstChild) {
@@ -9,19 +8,16 @@ const clearNode = node => {
   return node;
 };
 
-// const newPlayers = [['krzysiu', '🧑'], ['misiu', '👱‍']];
-const newPlayers = [];
-function addNewPlayer() {
-  const newPlayer = ['', ''];
-  newPlayer[0] = inputText.value;
-  inputText.value = '';
-  newPlayers[newPlayers.length] = newPlayer;
-  clearNode(playersList);
+const newPlayers = [['krzysiu', '🧑'], ['misiu', '👱‍']];
+// const newPlayers = [];
+
+const newPlayersToPlayersList = function () {
+  playersList.innerHTML = '';
   newPlayers.forEach(Item => {
     // Create new li element
     const li = document.createElement('li');
     li.className = 'playersListItem';
-    li.appendChild(document.createTextNode(`${Item[0]}${Item[1]}`));
+    li.appendChild(document.createTextNode(`${Item[1]} ${Item[0]} `));
 
     //Create delete button
     const deleteBtn = document.createElement('button');
@@ -31,10 +27,61 @@ function addNewPlayer() {
 
     //Add li element to list
     playersList.appendChild(li);
-  });
+  })
 }
+newPlayersToPlayersList();
 
-addPlayer.addEventListener('click', addNewPlayer);
+btnNumberOfPlayers.forEach(item => {
+  item.addEventListener('click', function () {
+    const numberOfPlayers = this.innerText;
+    if (newPlayers.length !== numberOfPlayers) {
+      if (newPlayers.length < numberOfPlayers) {
+        for (let i = newPlayers.length; i < numberOfPlayers; i++) {
+          const newPlayer = ['', ''];
+          newPlayers[newPlayers.length] = newPlayer;
+        }
+      }
+      else if (newPlayers.length > numberOfPlayers) {
+        newPlayers.splice(numberOfPlayers);
+      }
+    }
+    newPlayersToPlayersList();
+  });
+});
+
+// const btnDelete = [...document.querySelectorAll('.btnDelete')]
+// btnDelete.forEach(item => {
+//   item.addEventListener('click', function () {
+//     clearNode(this.parentNode);
+//     console.log(this.parentNode);
+//   })
+// });
+
+// function addNewPlayer() {
+//   const newPlayer = ['', ''];
+//   console.log(btnNumberOfPlayers);
+//   newPlayer[0] = inputText.value;
+//   inputText.value = '';
+//   newPlayers[newPlayers.length] = newPlayer;
+//   clearNode(playersList);
+//   newPlayers.forEach(Item => {
+//     // Create new li element
+//     const li = document.createElement('li');
+//     li.className = 'playersListItem';
+//     li.appendChild(document.createTextNode(`${Item[0]}${Item[1]}`));
+
+//     //Create delete button
+//     const deleteBtn = document.createElement('button');
+//     deleteBtn.className = 'btnDelete';
+//     deleteBtn.appendChild(document.createTextNode('Usuń'));
+//     li.appendChild(deleteBtn);
+
+//     //Add li element to list
+//     playersList.appendChild(li);
+//   });
+// }
+
+// addPlayer.addEventListener('click', addNewPlayer);
 
 function returnNewPlayers() {
   return newPlayers;
